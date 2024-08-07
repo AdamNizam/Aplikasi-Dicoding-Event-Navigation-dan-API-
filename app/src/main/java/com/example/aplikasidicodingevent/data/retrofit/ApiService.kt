7045214f97065cb663990c541d9777819eeb37ab
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-08-07 23:59:52 +0700

    Aplikasi Dicoding Event (Navigation dan API)

---
+++ app/src/main/java/com/example/aplikasidicodingevent/data/retrofit/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.aplikasidicodingevent.data.retrofit
+
+import com.example.aplikasidicodingevent.data.response.ListEventsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("events")
+    fun getEvents(
+        @Query("active") active: Int
+    ): Call<ListEventsResponse>
+}
+
